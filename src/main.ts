88914920c8461e43327a9b0f96ccc078744bcd2d
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-27 19:06:51 +0300

    a

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-// Optional property access
-type Customer = {
-  JoinedDate: Date;
-};
+// Object of countries
 
-function getCustomer(id: number): Customer | null | undefined {
-  return id === 0 ? null : { JoinedDate: new Date() };
+type Country = {
+  id: number,
+  name: string,
+  code: number
 }
 
-let customer = getCustomer(1)
-// ? <== this means in TS Optional property access 
-console.log(customer?.JoinedDate.getHours());
 
-// Optional element access 
-let number: number[] = [1, 2, 3, 4]
-number?.[1]
+let countries: Country = {
+  id: 1,
+  name: "Somalia",
+  code: +252
+}
+
+
+console.log(countries);
 
